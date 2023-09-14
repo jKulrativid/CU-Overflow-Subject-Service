43c98c46a86edb50939aa5fddb6ef3678b82e749
K. Jetnipit <jetnipit54@gmail.com>
2023-09-14 17:56:05 +0700

    feat: add entity models

--- src/service/entity.go
+++ src/service/entity.go
@@ -1,1 +1,28 @@
 package service
+
+type Post struct {
+}
+
+type File struct {
+}
+
+type Instructor struct {
+	Id          int64
+	FullName    string
+	Email       string
+	PhoneNumber string
+	Website     string
+	Degree      string
+}
+
+type Subject struct {
+	Id            int64
+	Name          string
+	Semester      int64
+	Year          int64
+	Description   string
+	Prerequisites []Subject
+	Instructors   []Instructor
+	Posts         []Post
+	Files         []File
+}
