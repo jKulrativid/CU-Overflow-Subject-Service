ec57cb4adf1c048541819d4cebf25f89edae2f05
K. Jetnipit <jetnipit54@gmail.com>
2023-09-27 20:09:04 +0700

    feat: update instruction repository

--- src/app/repository/repository.go
+++ src/app/repository/repository.go
@@ -6,7 +6,7 @@ type InstructorRepository interface {
 }
 
 type SubjectRepository interface {
-	PaginateSubjects(query map[string]interface{}) (*entity.PaginationMetadata, []*entity.Subject, error)
+	PaginateSubjects(pageNumber int64, query map[string]interface{}) (*entity.PaginationMetadata, []*entity.Subject, error)
 	FindSubjectById(id int64) (*entity.Subject, error)
 	CreateSubject(subject *entity.Subject) error
 	UpdateSubject(subject *entity.Subject) error
